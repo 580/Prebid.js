27bb26a352f61e176ced0b1ea70f63e279710617
Iries Hu <irieshu@192.168.16.103>
2023-12-14 20:26:30 +0800

    Adding getAuctionByAdUnitCode function

--- src/auctionManager.js
+++ src/auctionManager.js
@@ -21,7 +21,7 @@
 
 import { uniques, flatten, logWarn } from './utils.js';
 import { newAuction, getStandardBidderSettings, AUCTION_COMPLETED } from './auction.js';
-import {find} from './polyfill.js';
+import {find, findLast} from './polyfill.js';
 import {AuctionIndex} from './auctionIndex.js';
 import CONSTANTS from './constants.json';
 import {useMetrics} from './utils/perfMetrics.js';
