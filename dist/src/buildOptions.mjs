a91c8311afe498cbb52d74324c0c78b84735dbd0
iHu <ihu@yollamedia.com>
2026-06-17 20:46:10 +0800

    Include buildOptions.mjs for test runner

---
+++ dist/src/buildOptions.mjs
@@ -0,0 +1,15 @@
+export default {
+  "pbGlobal": "YMPB",
+  "defineGlobal": true,
+  "features": {
+    "NATIVE": true,
+    "VIDEO": true,
+    "UID2_CSTG": true,
+    "GREEDY": false,
+    "AUDIO": true,
+    "LOG_NON_ERROR": true,
+    "LOG_ERROR": true
+  },
+  "distUrlBase": "https://cdn.jsdelivr.net/npm/prebid.js@10.29.1/dist/chunks/",
+  "skipCalls": {}
+}
